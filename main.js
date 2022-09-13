e0218f8ecd38204e5dbb80830292546b646af4fd
37148149 <109961338+37148149@users.noreply.github.com>
2022-09-13 17:25:39 +0500

    First comment

---
+++ main.js
@@ -0,0 +1,11 @@
+// changing scroll-bar
+
+let nav = document.querySelector(".navbar");
+console.log(document)
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
